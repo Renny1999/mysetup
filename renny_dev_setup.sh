72148a7cfcfbe5240cb51bd320b6f5e41640e24a
Renny1999 <rennyhong1103@gmail.com>
2021-09-28 23:48:14 -0700

    updated vim plug-in manager to vimplug

--- renny_dev_setup.sh
+++ renny_dev_setup.sh
@@ -9,14 +9,10 @@
 cp ./.tmux.conf ~ &&
 
 #### setup vim
-## for vim, add pathogen
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim &&
+## for vim, add vimplug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-## install plugins for vim
-# solarized dark    
-git clone git://github.com/altercation/vim-colors-solarized.git && \
-mv vim-colors-solarized ~/.vim/bundle && \
 
 # set up .vimrc
 cp ./.vimrc ~ 
