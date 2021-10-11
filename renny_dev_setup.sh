643ef32c9e354350c956c03d1fcccfb411ee3292
Renny Hong <rennyhong1103@gmail.com>
2021-10-10 23:31:09 -0700

    added section for zsh plug-ins

--- renny_dev_setup.sh
+++ renny_dev_setup.sh
@@ -6,7 +6,7 @@
 
 #### setup tmux
 # setup .tmux.conf
-cp ./.tmux.conf ~ &&
+cp ./.tmux.conf ~ 
 
 #### setup vim
 ## for vim, add vimplug
@@ -16,5 +16,9 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 
 # set up .vimrc
 cp ./.vimrc ~ 
+
+# zsh plug-ins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
 
 
