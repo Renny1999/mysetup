07eef4becf66d5e0ae34d5b648506b74d98b2ddc
Renny1999 <rennyhong1103@gmail.com>
2021-10-12 22:25:24 -0700

    made the dotfiles sym-links

--- renny_dev_setup.sh
+++ renny_dev_setup.sh
@@ -4,21 +4,30 @@
 # install vim?
 # install tmux
 
+### add this directory to .bashrc and .zshrc
+echo "export MYSETUP=$(pwd)" | tee -a ~/.bashrc
+echo "export MYSETUP=$(pwd)" | tee -a ~/.zshrc
+source ~/.zshrc
+source ~/.bashrc
+
 #### setup tmux
-# setup .tmux.conf
-cp ./.dotfiles/.tmux.conf ~ 
+# setup .tmux.conf by deleting the existing and create a symlink to the new one
+rm ~/.tmux.conf
+ln -s $(pwd)/.dotfiles/.tmux.conf ~/.tmux.conf
+
 
 #### setup vim
 ## for vim, add vimplug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-
 # set up .vimrc
-cp ./.dotfiles/.vimrc ~ 
+rm ~/.vimrc
+ln -s $(pwd)/.dotfiles/.vimrc ~/.vimrc
 
 # zsh plug-ins
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+
+#git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+#echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
 
 
--- set_path.sh
+++ set_path.sh
@@ -3,7 +3,7 @@
 #### this script exports environmental variables to .bashrc and .zshrc
 
 #### .bashrc
-touch ~/.bashrc && echo "export PATH=\$PATH:$PWD/mybin" >> ~/.bashrc
+touch ~/.bashrc && echo "export PATH=\$PATH:$PWD/mybin" | tee -a  ~/.bashrc
 
 #### .zshrc
-touch ~/.zshrc && echo "export PATH=\$PATH:$PWD/mybin" >> ~/.zshrc
+touch ~/.zshrc && echo "export PATH=\$PATH:$PWD/mybin" | tee -a ~/.zshrc
